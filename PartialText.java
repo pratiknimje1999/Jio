9ab9a88aec9a252836c16e5119274144cddfa604
pratiknimje1999 <132550569+pratiknimje1999@users.noreply.github.com>
2023-05-31 23:19:47 +0530

    Add files via upload

---
+++ PartialText.java
@@ -0,0 +1,35 @@
+package Webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class PartialText {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.jio.com/help/home#/");
+		driver.findElement(By.partialLinkText("Mobi")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("True")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("JioFi")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("Busi")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("Sh")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("Ap")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("Supp")).click();
+		Thread.sleep(2000);
+		
+		
+		
+
+	}
+
+}
